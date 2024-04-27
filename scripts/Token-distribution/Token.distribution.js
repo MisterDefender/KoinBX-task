1eb49c0638745e13db4eba118a2b1b64ba5d6abc
MisterDefender <nikhilmukati123@gmail.com>
2024-04-27 19:23:31 +0530

    feat: pre mint the max deposit amount to owner

--- scripts/Token-distribution/Token.distribution.js
+++ scripts/Token-distribution/Token.distribution.js
@@ -11,6 +11,13 @@ async function main() {
   const kBXToken = await ethers.getContractAt("KoinBX", tokenAddress.address);
   const tokenOwner = owner.address;
 
+  // // Max minting the balance before depositing
+  // let maxAmount = 10000 * 999;
+  // const mintTx = await (await kBXToken
+  // .connect(owner)
+  // .mintToken(tokenOwner, ethers.parseUnits(maxAmount.toString(), 18))).wait();
+  // console.log("mint tx status: ", mintTx.status);
+
   const jsonData = fs.readFileSync("PubPvt.json", "utf8");
 
   try {
@@ -19,8 +26,10 @@ async function main() {
     const addresses = Object.keys(keyPairs).map((address) => address);
 
     var i = 0;
+    let totalDeposits = 0;
     while (i < addresses.length) {
       // console.log(i);
+
       for (let j = i + 1; j <= i + 10; j++) {
         let set = 0;
         if (j % 7 == 0) {
@@ -28,16 +37,13 @@ async function main() {
           // console.log("J: ", j);
           let amount = Math.floor(Math.random() * 999) + 1; // Random amount between 1 to 999
           amount = ethers.parseUnits(amount.toString(), 18); // random no * 10^18 decimals
-
-          const mintTx = await await kBXToken
-            .connect(owner)
-            .mintToken(tokenOwner, amount);
+          totalDeposits++;
 
           const tx = await (
             await kBXToken.connect(owner).transfer(addresses[j], amount)
           ).wait();
           console.log(
-            `${amount} of token distributed to ${to} with txn status: ${tx.status}\n``${amount} of token distributed to ${to} with txn hash: ${tx.transactionHash}`
+            `${amount} of token distributed to ${addresses[j]} with txn status: ${tx.status}\n txn hash: ${tx.transactionHash}`
           );
 
           set = 1;
@@ -46,7 +52,7 @@ async function main() {
       }
       i += 10;
     }
-
+    console.log("Total deposits made are: " + totalDeposits);
     // for (const address of Object.keys(keyPairs)) {
     //   let amount = Math.floor(Math.random() * 999) + 1; // Random amount between 1 to 999
     //   amount = ethers.parseUnits(amount.toString(), 18); // random no * 10^18 decimals
