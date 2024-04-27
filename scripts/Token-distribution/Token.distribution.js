e155cfa9ee90fc7d94740e707804db9c80bc4e0d
MisterDefender <nikhilmukati123@gmail.com>
2024-04-27 14:18:07 +0530

    feat: token distribution in first multiple of 7 in range of 10

--- scripts/Token-distribution/Token.distribution.js
+++ scripts/Token-distribution/Token.distribution.js
@@ -4,29 +4,55 @@ const fs = require("fs");
 async function distributeTokens(tokenOwner, to, amount) {
   const tokenAddress = await deployments.get("KoinBX");
   const kBXToken = await ethers.getContractAt("KoinBX", tokenAddress.address);
-  const mintTx = await await kBXToken
-    .connect(tokenOwner)
-    .mintToken(tokenOwner, amount);
-  const tx = await (
-    await kBXToken.connect(tokenOwner).transfer(to, amount)
-  ).wait();
-  console.log(
-    `${amount} of token distributed to ${to} with txn status: ${tx.status}`
-  );
 }
 async function main() {
   const [owner] = await ethers.getSigners();
+  const tokenAddress = await deployments.get("KoinBX");
+  const kBXToken = await ethers.getContractAt("KoinBX", tokenAddress.address);
+  const tokenOwner = owner.address;
+
   const jsonData = fs.readFileSync("PubPvt.json", "utf8");
 
   try {
     const keyPairs = JSON.parse(jsonData);
 
-    for (const address of Object.keys(keyPairs)) {
-      let amount = Math.floor(Math.random() * 999) + 1; // Random amount between 1 to 999
-      amount = ethers.parseUnits(amount.toString(), 18); // random no * 10^18 decimals
+    const addresses = Object.keys(keyPairs).map((address) => address);
 
-      await distributeTokens(owner, address, amount);
+    var i = 0;
+    while (i < addresses.length) {
+      // console.log(i);
+      for (let j = i + 1; j <= i + 10; j++) {
+        let set = 0;
+        if (j % 7 == 0) {
+          // Multiple of 7
+          // console.log("J: ", j);
+          let amount = Math.floor(Math.random() * 999) + 1; // Random amount between 1 to 999
+          amount = ethers.parseUnits(amount.toString(), 18); // random no * 10^18 decimals
+
+          const mintTx = await await kBXToken
+            .connect(owner)
+            .mintToken(tokenOwner, amount);
+
+          const tx = await (
+            await kBXToken.connect(owner).transfer(addresses[j], amount)
+          ).wait();
+          console.log(
+            `${amount} of token distributed to ${to} with txn status: ${tx.status}\n``${amount} of token distributed to ${to} with txn hash: ${tx.transactionHash}`
+          );
+
+          set = 1;
+          break; //break since taking only first multiple
+        }
+      }
+      i += 10;
     }
+
+    // for (const address of Object.keys(keyPairs)) {
+    //   let amount = Math.floor(Math.random() * 999) + 1; // Random amount between 1 to 999
+    //   amount = ethers.parseUnits(amount.toString(), 18); // random no * 10^18 decimals
+
+    //   await distributeTokens(owner, address, amount);
+    // }
   } catch (error) {
     console.error("Error parsing or reading JSON data:", error);
   }
